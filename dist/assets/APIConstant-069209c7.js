6a17c458aa9f9830ece9a0b2723b52b71dec0487
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-06-21 22:18:01 +0200

    feat:coonmit

---
+++ dist/assets/APIConstant-069209c7.js
@@ -0,0 +1,1 @@
+import{h as o,z as e}from"./index-13ef8100.js";const s="https://backend.mticket.co.mz/api",c="https://backend.mticket.co.mz",r="https://backend.mticket.co.mz/storage/";function l(){o.post(`${s}/logout`).then(t=>{console.log(t),localStorage.removeItem("token"),localStorage.removeItem("user"),e.replace("/login")})}export{c as a,s as b,l,r as s};
