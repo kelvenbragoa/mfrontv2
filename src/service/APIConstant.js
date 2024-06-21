692038aff11cf7cf89b34316a7bee0e539a3c2d9
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-06-21 22:16:37 +0200

    feat:coonmit

--- src/service/APIConstant.js
+++ src/service/APIConstant.js
@@ -3,13 +3,13 @@ import router from './../router/index';
 import { useRouter } from 'vue-router';
 
 
-export const baseURL = 'http://127.0.0.1:8001/api';
-export const styleURL = 'http://127.0.0.1:8001';
-export const storageURL = 'http://127.0.0.1:8001/storage/';
+// export const baseURL = 'http://127.0.0.1:8001/api';
+// export const styleURL = 'http://127.0.0.1:8001';
+// export const storageURL = 'http://127.0.0.1:8001/storage/';
 
-// export const baseURL = 'https://backend.mticket.co.mz/api';
-// export const styleURL = 'https://backend.mticket.co.mz';
-// export const storageURL = 'https://backend.mticket.co.mz/storage/';
+export const baseURL = 'https://backend.mticket.co.mz/api';
+export const styleURL = 'https://backend.mticket.co.mz';
+export const storageURL = 'https://backend.mticket.co.mz/storage/';
 
 
 
